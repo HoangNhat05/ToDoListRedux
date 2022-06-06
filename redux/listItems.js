94544278669fbb87db8549862f501a6353b9e716
Pham Vuong Hoang Nhat <hoangnhat@tgm.vn>
2022-06-06 17:40:48 +0700

    add_update_delete_UI

--- redux/listItems.js
+++ redux/listItems.js
@@ -1,75 +1,109 @@
-import React from 'react';
-import { StyleSheet,
-     Text, 
-     SafeAreaView ,
-     TextInput,
-     View,
-     Button, 
-     TouchableOpacity,
-     FlatList,
-     Animated,
-     StatusBar
-    } from 'react-native';
-import store from './store';
-import reducer from './reducer'
-import {useSelector} from 'react-redux'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+  View,
+  Button,
+  TouchableOpacity,
+  FlatList,
+  Animated,
+  StatusBar,
+} from "react-native";
+import store from "./store";
+import reducer from "./reducer";
+import { useSelector } from "react-redux";
 
 const ListItems = () => {
+  const value = useSelector((state) => state) || [];
 
+  console.log(value);
+  const renderItem = ({ item }) => (
+    <View style={styles.viewItem}>
+      <Text style={styles.item}>{item.text}</Text>
 
-    const value = useSelector(state => state) || []
-  
-    console.log(value)
-    const renderItem = ({ item }) => (
-      <Text style={styles.item}>{item.text}</Text>  
-    )
-    
-    
-    return(
-      <View style={{marginTop: 100}}>
-        <Text style={{fontSize:20, justifyContent:'center',alignSelf:'center',}}>{"Danh sách việc cần làm"}</Text> 
-        <FlatList
+      <TouchableOpacity style={styles.update}>
+        <Text style={styles.textStyle}>Sửa</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.delete}>
+        <Text style={styles.textStyle}>Xoá</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={{ marginTop: 100 }}>
+      <Text
+        style={{ fontSize: 20, justifyContent: "center", alignSelf: "center" }}
+      >
+        {"Danh sách việc cần làm"}
+      </Text>
+      <FlatList
         data={value}
         renderItem={renderItem}
-        keyExtractor={item => item.key}
+        keyExtractor={(item) => item.key}
       />
-     
-        {/* {value.map(element => <Text style={styles.item}>{element}</Text>)} */}
-        {/* <Text style={styles.item}>{value}</Text>         */}
-      </View>
-    )
-    
 
-}
+      {/* {value.map(element => <Text style={styles.item}>{element}</Text>)} */}
+      {/* <Text style={styles.item}>{value}</Text>         */}
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      width:'60%',
-      margin: 12,
-      padding: 10,
-      borderBottomWidth:1
-    },
-    view_add:{
+  input: {
+    height: 40,
+    width: "60%",
+    margin: 12,
+    padding: 10,
+    borderBottomWidth: 1,
+  },
+  view_add: {
     marginTop: 40,
-    flexDirection:'row',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewItem: {
+    flexDirection: "row",
+    flex: 10,
+    margin: 5,
+  },
+  item: {
+    //height:30,
+    flex: 8,
+    marginStart: 30,
+    marginTop: 15,
+    marginEnd: 15,
+  },
+  button_add: {
+    height: 30,
+    width: 60,
+    borderColor: "blue",
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  update: {
+    flex: 1,
+    backgroundColor: "blue",
+    marginRight: 3,
+    borderRadius: 10,
     justifyContent:'center',
     alignItems:'center'
-    },
-    item:{
-        //height:30,
-        marginStart:30,
-        marginTop:15, 
-        marginEnd:15
-    },
-    button_add:{
-      height: 30,
-      width:60,
-      borderColor:'blue',
-      borderWidth:2,
-      borderRadius:10,
-      justifyContent:'center',
-      alignItems:'center' 
-    },
-   
-  });
+  },
+  delete: {
+    flex: 1,
+    backgroundColor: "red",
+    marginLeft: 3,
+    borderRadius: 10,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textStyle: {
+    color: "white",
+  },
+});
 export default ListItems;
